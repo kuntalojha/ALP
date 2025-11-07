e2da26d46524781d333945be620973d5b9d28c78
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-11-07 12:40:35 +0530

    fixed bug

--- docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import PdfViewer from './components/PdfViewer.vue'
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('PdfViewer', PdfViewer)
-  }
-}
